f12231eaa900fe053bbd60dd618ad5e94c314752
MariamTchelidze <mariam.tch1996@gmail.com>
2025-03-06 18:55:15 +0400

    added javascript file

---
+++ script.js
@@ -0,0 +1,10 @@
+// Javascript file
+let a = 2;
+function square (num) {
+    var ans = num * num;
+    return ans;
+}
+var square2 = square(a);
+var square4 = square(4);
+console.log(square2);
+alert(square4);
