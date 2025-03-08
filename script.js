44e5cf9bd4644630d8add456654f8fbf5c6903d6
MariamTchelidze <mariam.tch1996@gmail.com>
2025-03-08 20:59:05 +0400

    changes in njs files

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// // Javascript file
-// let a = 2;
-// function square (num) {
-//     var ans = num * num;
-//     return ans;
-// }
-// var square2 = square(a);
-// var square4 = square(4);
-// console.log(square2);
-// alert(square4);
+// Javascript file
+let a = 2;
+function square (num) {
+    var ans = num * num;
+    return ans;
+}
+var square2 = square(a);
+var square4 = square(4);
+console.log(square2);
+alert(square4);
 
 
 // function substract (a, b){
@@ -17,40 +17,33 @@
 // console.log(substract(7, 150));
 
 
-// function sum (x, y){
-//     return x + y;
-// }
-// console.log(sum(3, 4));
-// console.log(sum(23, 83));
+function sum (x, y){
+    return x + y;
+}
+console.log(sum(3, 4));
+console.log(sum(23, 83));
 
 
-// function division(s, h){
-//     return h / s;
-// }
-// console.log(division(8, 16));
-// console.log(division(4, 32));
+function division(s, h){
+    return h / s;
+}
+console.log(division(8, 16));
+console.log(division(4, 32));
 
-// function multiply(g, j){
-//     return g * j;
-// }
-// console.log(multiply(2, 45));
-// console.log(multiply(5, 6));
-
-
-
-// var x =7;
-// function getName(){
-//     console.log("Namaste Javascript");
-// }
-// getName(); // თუ ამას გადავიტანთ ზემოთ მაგალითად მე-40 ხაზზე კონსოლში მაინც გამოჩნდება  "Namaste Javascript"
-// console.log(x);  // თუ ამნას გადავიტანთ 39 ხაზზე, მაშინ გვექნება  undefuned, 
-// //ამ ორივეს გადატანა ფუნქციის დეკლარილებამდე, რა შედეგსაც იძლევა ამას ეწოდება hosting
+function multiply(g, j){
+    return g * j;
+}
+console.log(multiply(2, 45));
+console.log(multiply(5, 6));
 
 
 getName();
 console.log(x);
 
-var x = 7;
-var getname = () =>{
-    console.log("Namaste Javascript")
+var x =7;
+function getName(){
+    console.log("Namaste Javascript");
 }
+getName(); // თუ ამას გადავიტანთ ზემოთ მაგალითად მე-40 ხაზზე კონსოლში მაინც გამოჩნდება  "Namaste Javascript"
+console.log(x);  // თუ ამნას გადავიტანთ 39 ხაზზე, მაშინ გვექნება  undefuned, 
+//ამ ორივეს გადატანა ფუნქციის დეკლარილებამდე, რა შედეგსაც იძლევა ამას ეწოდება hosting
